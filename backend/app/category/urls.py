97a068ed860d1668674993352257f8aadd9827c0
paczaj-code <marcinpaczkowski@icloud.com>
2023-02-07 10:37:45 +0100

    Remove get by id

--- backend/app/category/urls.py
+++ backend/app/category/urls.py
@@ -1,16 +1,10 @@
 from django.urls import path
-from .views import (CategoryListAPIView, CategoryDetailAPIViewByPkOrSlug)
+from .views import (CategoryListAPIView, CategoryDetailAPIViewBySlug)
 
 urlpatterns = [
-    # endpoint for categpry list
     path('category/', CategoryListAPIView.as_view(), name='category-list'),
-    # endpoint for categpry detail by category id
-    path('category/<int:pk>', CategoryDetailAPIViewByPkOrSlug.as_view(),
-         name='category-by-id'),
-    # endpoint for categpry detail by category slug
-    path('category/<str:slug>', CategoryDetailAPIViewByPkOrSlug.as_view(),
+    path('category/<str:slug>', CategoryDetailAPIViewBySlug.as_view(),
          name='category-by-slug'),
 ]
-
 
 app_name = 'category'
--- backend/app/category/views.py
+++ backend/app/category/views.py
@@ -1,8 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-# from django.contrib import messages
 from django.http import Http404
-
 from rest_framework import status
 from .serializers import (PublicCategoryListSerializer,
                           PublicCategoryDetailSerializer)
@@ -14,10 +12,6 @@ class CategoryListAPIView(APIView):
     """View for category lists with recursive subcategories"""
 
     def get(self, request, *args, **kwargs):
-        # TODO resolve problem with filtering by is_enebled in subcategories
-        # categories = Category.objects.filter(category_type='P',
-        #                                      is_enabled=True, parent__isnull=True).order_by('id')
-
         categories = Category.objects.raw('''
             WITH
                 sub_query
@@ -63,14 +57,8 @@ class CategoryListAPIView(APIView):
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 
-class CategoryDetailAPIViewByPkOrSlug(APIView):
+class CategoryDetailAPIViewBySlug(APIView):
     """View for category details"""
-
-    def get_object_by_pk(self, pk):
-        try:
-            return Category.objects.get(pk=pk, is_enabled=True, category_type='P',)
-        except Category.DoesNotExist:
-            raise Http404
 
     def get_object_by_slug(self, slug):
         try:
@@ -79,11 +67,7 @@ class CategoryDetailAPIViewByPkOrSlug(APIView):
             raise Http404
 
     def get(self, request, *args, **kwargs):
-        pk = self.kwargs.get('pk', None)
         slug = self.kwargs.get('slug', None)
-        if pk:
-            obj = self.get_object_by_pk(pk)
-        else:
-            obj = self.get_object_by_slug(slug)
+        obj = self.get_object_by_slug(slug)
         serializer = PublicCategoryDetailSerializer(obj)
         return Response(serializer.data, status=status.HTTP_200_OK)
