5fd35e5c112b02359414ea2f419ec7b9cd210b9c
paczaj-code <marcinpaczkowski@icloud.com>
2023-03-08 00:49:45 +0100

    Add media urls

--- backend/app/app/urls.py
+++ backend/app/app/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from category import urls
-from blog import urls
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('api/', include('category.urls')),
@@ -24,3 +24,10 @@ urlpatterns = [
     path('api/', include('terminology.urls')),
     path('api/', include('practise.urls')),
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# if settings.DEBUG:
+# urlpatterns += static(settings.MEDIA_URL,
+#                       document_root=settings.MEDIA_ROOT)
+print(settings.MEDIA_URL)
