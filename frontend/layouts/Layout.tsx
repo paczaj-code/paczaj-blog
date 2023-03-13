fbb9dd3fccdd05a1e6566d608adc491695877cfb
paczaj-code <marcinpaczkowski@icloud.com>
2023-03-13 01:08:44 +0100

    Change layout

--- frontend/layouts/Layout.tsx
+++ frontend/layouts/Layout.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { classNameModifiers } from 'utils/utils';
 
-interface MainLayoutTypes {
+interface LayoutTypes {
   children: React.ReactNode;
   layout_prefix?: string;
   layout_modifier?: string;
 }
 
-const Layout: React.FC<MainLayoutTypes> = ({
+const Layout: React.FC<LayoutTypes> = ({
   children,
   layout_prefix,
   layout_modifier,
