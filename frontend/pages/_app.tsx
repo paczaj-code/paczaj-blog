b5b864f4b68d927212ce236cecd38147c204c63b
paczaj-code <marcinpaczkowski@icloud.com>
2023-03-13 01:08:55 +0100

    Cleaning code

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -3,7 +3,6 @@ import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import Header from '@/components/Header/Header';
 import 'prismjs/themes/prism-tomorrow.css';
-import Layout from 'layouts/Layout';
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
