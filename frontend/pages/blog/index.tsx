ea588bfebfa53abdb0ad614b7b4ef76eb7f672c9
paczaj-code <marcinpaczkowski@icloud.com>
2023-03-13 01:10:21 +0100

    Refactor component

--- frontend/pages/blog/index.tsx
+++ frontend/pages/blog/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
+import Head from 'next/head';
 import parse from 'html-react-parser';
 import { useRouter } from 'next/router';
 import Section from '@/components/Section/Section';
 import ItemCard from '@/components/UI/Card/ItemCard';
 import Icon from '@/components/Icon/Icon';
-import Button from '@/components/UI/Button/Button';
 import Heading from '@/components/UI/Heading/Heading';
-
+import Layout from 'layouts/Layout';
 interface CategoryTypes {
   id: number;
   name: string;
@@ -26,45 +26,37 @@ const Articles: React.FC<CategoriesTypes> = ({ categories }) => {
   const router = useRouter();
   return (
     <>
-      {categories &&
-        categories.map((category, index) => (
-          <Section
-            key={category.id}
-            section_prefix="category"
-            section_modifier={category.slug}
-            section_extra_classes={['fade-in-top']}
-            seection_style={{ animationDelay: `${50 * index}ms` }}
-          >
-            <div className="section__wrapper">
-              <div className={`section__image--${category.slug}`}></div>
-              <div className="section__description">
-                <Heading headingLevel="h3" heading_label={category.name} />
+      <Head>
+        <title>Blog</title>
+      </Head>
+      <Layout layout_modifier="full">
+        {categories &&
+          categories.map((category, index) => (
+            <Section key={category.id} section_prefix="category">
+              <div className="category__description">
+                <Heading headingLevel="h2" heading_label={category.name} />
                 <div>{parse(category.description!)}</div>
               </div>
-            </div>
-            {category.sub_categories.length &&
-              category.sub_categories.map((subcategory, index) => (
-                <ItemCard
-                  key={index}
-                  itemCard_prefix="category"
-                  itemCard_modifier={subcategory.slug}
-                  onClick={() => router.push(`/blog/${subcategory.slug}`)}
-                >
-                  {subcategory.icon && (
-                    <Icon icon_prefix="item-card" icon={subcategory.icon} />
-                  )}
-                  <h3>{subcategory.name}</h3>
-                  <Button button_type="button" button_prefix="item-card">
-                    <span className="item-card__button__text">paczaj</span>
-                    <Icon
-                      icon="icon-circle-right"
-                      icon_prefix="item-card__button"
-                    />
-                  </Button>
-                </ItemCard>
-              ))}
-          </Section>
-        ))}
+              {/* </div> */}
+              <div className="category__cards">
+                {category.sub_categories.length &&
+                  category.sub_categories.map((subcategory, index) => (
+                    <ItemCard
+                      key={index}
+                      itemCard_prefix="category"
+                      // itemCard_modifier={subcategory.slug}
+                      onClick={() => router.push(`/blog/${subcategory.slug}`)}
+                    >
+                      {subcategory.icon && (
+                        <Icon icon_prefix="item-card" icon={subcategory.icon} />
+                      )}
+                      <h3>{subcategory.name}</h3>
+                    </ItemCard>
+                  ))}
+              </div>
+            </Section>
+          ))}
+      </Layout>
     </>
   );
 };
